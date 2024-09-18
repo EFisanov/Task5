df32f690e51780395e8b0c8aaf26b976e195188a
Evgeny Fisanov <E.Fisanov@compassplus.com>
2024-09-18 12:07:13 +0500

    [.i] (TXPG-9591) Произведён рефакторинг кода.

--- Server/src/main/java/org/example/Main.java
+++ Server/src/main/java/org/example/Main.java
@@ -20,53 +20,38 @@ import java.util.Iterator;
 
 public class Main {
     private final static String FILE_NAME_XML = "C:\\Users\\efisanov\\IdeaProjects\\Task5\\Server\\src\\main\\resources\\data.xml";
-    private static Socket clientSocket;
-    private static ServerSocket server;
-    private static BufferedReader in;
-    private static BufferedWriter out;
+    private static final int PORT = 8080;
 
     public static void main(String[] args) {
         MessagesDocument document = MessagesDocument.Factory.newInstance();
         MessagesDocument.Messages messages = document.addNewMessages();
 
-        try {
-            try {
-                server = new ServerSocket(8080);
-                System.out.println("Сервер запущен");
-                clientSocket = server.accept();
-                try {
-                    String word;
-                    do {
-                        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                        out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-                        word = in.readLine();
+        try (
+                ServerSocket server = new ServerSocket(PORT);
+                Socket clientSocket = server.accept();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))
+        ) {
+            System.out.println("Сервер запущен");
+            String incomingMessage;
+            do {
+                incomingMessage = br.readLine();
 
-                        if (word.equals("exit")) {
-
-                            out.write("Сервер закрыт.");
-                            out.flush();
-                            break;
-                        }
-                        System.out.println(word);
-
-                        MessagesDocument.Messages.Message message = messages.addNewMessage();
-                        message.setDate(convertTime(LocalDateTime.now()));
-                        message.setText(word);
-                        out.write("Получено сообщение: " + word + "\n");
-                        out.flush();
-                    } while (word.equals("exit")==false);
-
-                    serialisationToXml(FILE_NAME_XML, document);
-                    System.out.println("Количество принятых сообщений: " + countMessagesNumber(FILE_NAME_XML));
-                } finally {
-                    clientSocket.close();
-                    in.close();
-                    out.close();
+                if (incomingMessage.equals("exit")) {
+                    sendResponse("Сервер закрыт.", bw);
+                    System.out.println("Сервер закрыт.");
+                    break;
                 }
-            } finally {
-                System.out.println("Сервер закрыт.");
-                server.close();
-            }
+                System.out.println(incomingMessage);
+
+                MessagesDocument.Messages.Message message = messages.addNewMessage();
+                message.setDate(convertTime(LocalDateTime.now()));
+                message.setText(incomingMessage);
+                sendResponse("Получено сообщение: " + incomingMessage + "\n", bw);
+            } while (incomingMessage.equals("exit") == false);
+
+            serialisationToXml(FILE_NAME_XML, document);
+            System.out.println("Количество принятых сообщений: " + countMessagesNumber(FILE_NAME_XML));
         } catch (IOException | SAXException | ParserConfigurationException | XPathExpressionException e) {
             e.printStackTrace();
         }
@@ -116,5 +101,10 @@ public class Main {
         XPathExpression expr = xpath.compile("count(//mes:messages/mes:message)");
         Number result = (Number) expr.evaluate(doc, XPathConstants.NUMBER);
         return result.intValue();
+    }
+
+    private static void sendResponse(String response, BufferedWriter bw) throws IOException {
+        bw.write(response);
+        bw.flush();
     }
 }
